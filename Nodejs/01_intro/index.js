7e6f805b1804e9fa277c38f28b429f589780d015
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-10-13 19:27:40 +0545

    Started nodejs journey

---
+++ Nodejs/01_intro/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+// these global variables can be used anywhere in this module/file
+
+console.log(global);
+{
+    global.username = "govind kr yadav";
+    roll = "pas078bei015";
+}
+console.log(username);
+console.log(roll);
+
+console.log(global);
