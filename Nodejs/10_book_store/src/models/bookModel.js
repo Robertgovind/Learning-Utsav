8323e60c31a8cb369a02b3734eee8960a8bb82f0
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-10-21 19:29:16 +0545

    backend portion for bookstore project

---
+++ Nodejs/10_book_store/src/models/bookModel.js
@@ -0,0 +1,33 @@
+import { describe } from "node:test";
+import connecton from "./index.js";
+import { DataTypes } from "sequelize";
+
+export default connecton.define(
+  "books",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  },
+  { timestamps: false } // to disable createdAt and updatedAt
+);
